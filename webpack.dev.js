2c74951d8278a82c078286fc2c221f6b4a5a94a1
Hoyung <drwafflesxd@gmail.com>
2021-02-22 12:13:06 +0000

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-sequences */
+/* eslint-disable no-unused-expressions */
+const path = require('path');
+const common = require('./webpack.common.js');
+
+module.exports = common, {
+  mode: 'development',
+
+  devtool: 'source-map',
+
+  // https://webpack.js.org/configuration/dev-server/
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    noInfo: true,
+    open: true,
+    hot: true
+  },
+
+  module: {
+    rules: [{
+      enforce: 'pre',
+      test: /\.js$/,
+      loader: 'source-map-loader',
+    }],
+  },
+};
